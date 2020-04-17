00d3bc8806dd004596447ad72e3128bdec151433
Dawson Hamilton <dawsondhamilton@gmail.com>
2020-04-16 20:25:24 -0600

    first dawson commit

--- server.js
+++ server.js
@@ -21,4 +21,4 @@ mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/reactreadinglis
 // Start the API server
 app.listen(PORT, function () {
     console.log(`🌎  ==> API Server now listening on PORT ${PORT}!`);
-});
+}); 
