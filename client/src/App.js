0dbb9b61904720102abb4e5c47f48cc6047ceaed
Rockie Beatty <rockiebeatty@protonmail.com>
2020-04-18 14:52:57 -0600

    heroku deployment

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,20 @@
 import React from 'react';
 import Signup from "./components/Signup";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+
 
 function App() {
   return (
-    <Signup />
+    <Router>
+      <div>
+        <Switch>
+          <Route path="/" component={Signup} />
+          {/* <Route>
+            <NoMatch />
+          </Route> */}
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
