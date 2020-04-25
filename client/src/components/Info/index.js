a605c863f5fcefb5ea04dbb5a90f39689a2c32ab
Dawson Hamilton <dawsondhamilton@gmail.com>
2020-04-25 09:53:56 -0600

    adding the name and number input section to the form

---
+++ client/src/components/Info/index.js
@@ -0,0 +1,21 @@
+import React from "react";
+import 'bootstrap/dist/css/bootstrap.min.css';
+import { Form } from "react-bootstrap";
+
+function Info() {
+    return (
+        <Form>
+            <Form.Group controlId="formBasicEmail">
+                <Form.Label>Name: </Form.Label>
+                <Form.Control type="email" placeholder="Enter Name" />
+            </Form.Group>
+
+            <Form.Group controlId="formBasicPassword">
+                <Form.Label>Phone Number: </Form.Label>
+                <Form.Control type="password" placeholder="Enter Phone Number" />
+            </Form.Group>
+        </Form>
+    );
+}
+
+export default Info;
--- client/src/pages/GigSetup.js
+++ client/src/pages/GigSetup.js
@@ -6,64 +6,77 @@ import Time from "../components/Time";
 import Address from "../components/Address";
 import Comment from "../components/Comment";
 import 'bootstrap/dist/css/bootstrap.min.css';
-import { Button } from "react-bootstrap";
+import { Button, Form } from "react-bootstrap";
+import Info from "../components/Info";
 
 function GIGSETUP() {
     return (
         <Container fluid>
-            <br />
-            <Row className="datePicker">
-                <Col size="md-5 sm-12"></Col>
-                <Col size="md-2 sm-12">
-                    <h1>Date:</h1>
-                    <br />
-                    <Calender />
-                </Col>
-                <Col size="md-5 sm-12"></Col>
-            </Row>
-            <br />
-            <Row>
-                <Col size="md-5 sm-12"></Col>
-                <Col size="md-2 sm-12">
-                    <h1>Times: </h1>
-                    <br />
-                    <h3>From: </h3>
-                    <br />
-                    <Time />
-                    <br />
-                    <h3>To: </h3>
-                    <br />
-                    <Time />
-                </Col>
-                <Col size="md-5 sm-12"></Col>
-            </Row>
-            <br />
-            <br />
-            <Row>
-                <Col size="md-5 sm-12"></Col>
-                <Col size="md-2 sm-12">
-                    <h1>Address: </h1>
-                    <Address />
-                </Col>
-                <Col size="md-5 sm-12"></Col>
-            </Row>
-            <br />
-            <Row>
-                <Col size="md-5 sm-12"></Col>
-                <Col size="md-2 sm-12">
-                    <h1>Comment for the Gigger: </h1>
-                    <Comment />
-                </Col>
-                <Col size="md-5 sm-12"></Col>
-            </Row>
-            <Row>
-                <Col size="md-2 sm-12"></Col>
-                <Col size="md-8 sm-12">
-                    <Button variant="success" style={{ width: "350px", height: "50px" }}>GIG IT!</Button>{' '}
-                </Col>
-                <Col size="md-2 sm-12"></Col>
-            </Row>
-            <br />
+            <Form>
+                <br />
+                <Row className="datePicker">
+                    <Col size="md-5 sm-12"></Col>
+                    <Col size="md-2 sm-12">
+                        <h1>Personal Info: </h1>
+                        <br />
+                        <Info />
+                    </Col>
+                    <Col size="md-5 sm-12"></Col>
+                </Row>
+                <br />
+                <Row className="datePicker">
+                    <Col size="md-5 sm-12"></Col>
+                    <Col size="md-2 sm-12">
+                        <h1>Date:</h1>
+                        <br />
+                        <Calender />
+                    </Col>
+                    <Col size="md-5 sm-12"></Col>
+                </Row>
+                <br />
+                <Row>
+                    <Col size="md-5 sm-12"></Col>
+                    <Col size="md-2 sm-12">
+                        <h1>Times: </h1>
+                        <br />
+                        <h3>From: </h3>
+                        <br />
+                        <Time />
+                        <br />
+                        <h3>To: </h3>
+                        <br />
+                        <Time />
+                    </Col>
+                    <Col size="md-5 sm-12"></Col>
+                </Row>
+                <br />
+                <br />
+                <Row>
+                    <Col size="md-5 sm-12"></Col>
+                    <Col size="md-2 sm-12">
+                        <h1>Address: </h1>
+                        <Address />
+                    </Col>
+                    <Col size="md-5 sm-12"></Col>
+                </Row>
+                <br />
+                <Row>
+                    <Col size="md-5 sm-12"></Col>
+                    <Col size="md-2 sm-12">
+                        <h1>Comment for the Gigger: </h1>
+                        <Comment />
+                    </Col>
+                    <Col size="md-5 sm-12"></Col>
+                </Row>
+                <Row>
+                    <Col size="md-2 sm-12"></Col>
+                    <Col size="md-8 sm-12">
+                        <Button variant="success" style={{ width: "350px", height: "50px" }}>GIG IT!</Button>{' '}
+                    </Col>
+                    <Col size="md-2 sm-12"></Col>
+                </Row>
+                <br />
+            </Form>
         </Container>
     );
 }
