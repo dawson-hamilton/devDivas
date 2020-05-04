c132bf48f67a4e6899571b445ca7a0a3672ab439
Tom Ruff <ruffmantom@gmail.com>
2020-05-04 15:26:41 -0600

    getting logic for finding and sorting all gigs that match user email

--- client/src/components/Profile/index.js
+++ client/src/components/Profile/index.js
@@ -1,17 +1,27 @@
-import React, { Fragment } from "react";
+import React, { useEffect } from "react";
 import { useAuth0 } from "../../react-auth0-spa";
 import { Container, Col, Row } from "../../components/Grid";
 import "./style.css";
+import ProfileCard from "../../components/ProfileCard";
+import API from "../../utils/API";
 var stockUser = "https://st3.depositphotos.com/6672868/13701/v/450/depositphotos_137014128-stock-illustration-user-profile-icon.jpg";
 // <p>{user.email}</p>
 //<code>{JSON.stringify(user, null, 2)}</code>
 
 const Profile = () => {
     const { loading, user } = useAuth0();
-    // declaring if user has image or not
     var userImage;
+    var result;
 
+    // useeffect for component did mount
+    useEffect(() => {
 
+    })
+    // get all gigs and match with email
+    API.getGigs()
+        .then(res => {
+            res.filter(gig => { gig.email === user.email })
+        })
 
 
     if (loading || !user) {
@@ -21,11 +31,13 @@ const Profile = () => {
         </div>;
 
     } else {
+        // declaring if user has image or not
         if (user.picture) {
             userImage = user.picture
         } else {
             userImage = stockUser
         }
+        // returning page
         return (
             <Container>
 
@@ -33,6 +45,24 @@ const Profile = () => {
                     <img className="userImage" src={userImage} alt="Profile picture" />
 
                     <h2 className="userName">Welcome {user.name}</h2>
+                </div>
+                {/* not using row and col since react isnt liking it */}
+                <div className="cardSection">
+                    <div className="titleDiv">
+                        <h2 className="title">Your currently scheduled Gigs</h2>
+                    </div>
+                    <div className="cardContainer">
+                        {result.map(res => (
+                            <ProfileCard
+                                gigName="Santa Gig"
+                                number={res.phoneNum}
+                                bookDate={res.dateBooked}
+                                fromTime={res.startTime}
+                                toTime={res.endTime}
+                            />
+                        ))}
+                    </div>
+
                 </div>
 
 
