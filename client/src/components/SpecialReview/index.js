d7a48657374d5cd2131ef5ee93943a7850f3d071
Dawson Hamilton <dawsondhamilton@gmail.com>
2020-05-06 12:27:44 -0600

    adding the loading page wiht the lightning bolt in the center

--- client/src/components/SpecialReview/index.js
+++ client/src/components/SpecialReview/index.js
@@ -3,7 +3,7 @@ import "./style.css";
 
 function SpecialReview(props) {
     return (
-        <div className="div">
+        <div className="divs">
             <h2>{props.name}    <i class="fas fa-star" id={"star"}></i><i class="fas fa-star" id={"grey-star"}></i><i class="fas fa-star" id={"grey-star"}></i><i class="fas fa-star" id={"grey-star"}></i><i class="fas fa-star" id={"grey-star"}></i></h2>
             <p>{props.review}</p>
             <p>{props.date}</p>
