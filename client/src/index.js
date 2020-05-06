6fa11fdc40fae9bf9d0deafea4809506fc820c49
Jacob Dolph <jacob.dolph702@gmail.com>
2020-05-05 21:13:49 -0600

    fixed the token refresh issue

--- client/src/index.js
+++ client/src/index.js
@@ -20,7 +20,6 @@ ReactDOM.render(
     domain={config.domain}
     client_id={config.clientId}
     redirect_uri={window.location.origin}
-    audience={config.audience}
     onRedirectCallback={onRedirectCallback}
   >
     <App />
