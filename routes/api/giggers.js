6ed9d5b87ee4f4da25d5c83aba03ad6294f34808
ruffmantom <tomruffcodes@gmail.com>
2020-06-22 13:25:58 -0600

    trying to figure out what to get done

---
+++ routes/api/giggers.js
@@ -0,0 +1,4 @@
+const router = require("express").Router();
+const giggersController = require("../../controllers/giggersController");
+
+// need to match "/api/artists"
--- routes/api/index.js
+++ routes/api/index.js
@@ -1,7 +1,9 @@
 const router = require("express").Router();
 const gigRoutes = require("./gigs");
+const giggerRoutes = require("./giggers");
 
 // gig routes
 router.use("/gigs", gigRoutes);
+router.use("/artists", giggerRoutes);
 
 module.exports = router;
